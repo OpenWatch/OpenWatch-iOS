ecbc8e9df138875df3e516cb56732888730aef14
Chris Ballinger <chrisballinger@gmail.com>
2012-11-12 17:04:47 -0800

    Initial Commit

---
+++ OpenWatch/OWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OWAppDelegate.h
+//  OpenWatch
+//
+//  Created by Christopher Ballinger on 11/12/12.
+//  Copyright (c) 2012 OpenWatch FPC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
