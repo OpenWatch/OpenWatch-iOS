ff5216516ba6444b80634136912f363be1eeec78
Chris Ballinger <chrisballinger@gmail.com>
2012-11-13 16:59:59 -0800

    Add preview view

---
+++ OpenWatch/OWVideoPreviewViewController.h
@@ -0,0 +1,13 @@
+//
+//  OWVideoPreviewViewController.h
+//  OpenWatch
+//
+//  Created by Christopher Ballinger on 11/13/12.
+//  Copyright (c) 2012 OpenWatch FPC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWVideoPreviewViewController : UIViewController
+
+@end
---
+++ OpenWatch/OWVideoPreviewViewController.m
@@ -0,0 +1,29 @@
+//
+//  OWVideoPreviewViewController.m
+//  OpenWatch
+//
+//  Created by Christopher Ballinger on 11/13/12.
+//  Copyright (c) 2012 OpenWatch FPC. All rights reserved.
+//
+
+#import "OWVideoPreviewViewController.h"
+
+@interface OWVideoPreviewViewController ()
+
+@end
+
+@implementation OWVideoPreviewViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
