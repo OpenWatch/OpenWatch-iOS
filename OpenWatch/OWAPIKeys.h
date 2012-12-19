066e92dcbe62c053d1bc319ae3c8ce1d8c472149
Chris Ballinger <chrisballinger@gmail.com>
2012-12-19 17:13:18 -0500

    Add some files

---
+++ OpenWatch/OWAPIKeys.h
@@ -0,0 +1,14 @@
+//
+//  OWAPIKeys.h
+//  OpenWatch
+//
+//  Created by Christopher Ballinger on 12/19/12.
+//  Copyright (c) 2012 OpenWatch FPC. All rights reserved.
+//
+
+#ifndef OpenWatch_OWAPIKeys_h
+#define OpenWatch_OWAPIKeys_h
+
+
+
+#endif
---
+++ OpenWatch/OWCheckpoints.h
@@ -0,0 +1,14 @@
+//
+//  OWCheckpoints.h
+//  OpenWatch
+//
+//  Created by Christopher Ballinger on 12/19/12.
+//  Copyright (c) 2012 OpenWatch FPC. All rights reserved.
+//
+
+#ifndef OpenWatch_OWCheckpoints_h
+#define OpenWatch_OWCheckpoints_h
+
+
+
+#endif
