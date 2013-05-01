349c4f678dc9cdfeca114108eb7a057a74aae179
Chris Ballinger <chrisballinger@gmail.com>
2013-05-01 13:04:56 -0700

    Add password field to main screen

--- OpenWatch/OWFancyLoginViewController.m
+++ OpenWatch/OWFancyLoginViewController.m
@@ -8,13 +8,14 @@
 
 #import "OWFancyLoginViewController.h"
 #import "OWUtilities.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface OWFancyLoginViewController ()
 
 @end
 
 @implementation OWFancyLoginViewController
-@synthesize backgroundImageView, logoView, blurbLabel, emailField, startButton;
+@synthesize backgroundImageView, logoView, blurbLabel, emailField, startButton, scrollView, passwordField;
 
 
 - (id)init
@@ -22,8 +23,10 @@
     self = [super init];
     if (self) {
         self.emailField = [[UITextField alloc] init];
+        self.emailField.delegate = self;
         self.blurbLabel = [[UILabel alloc] init];
         self.logoView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"openwatch-light.png"]];
+        self.scrollView = [[UIScrollView alloc] init];
         self.startButton = [OWUtilities bigGreenButton];
     }
     return self;
@@ -32,25 +35,31 @@
 - (void) loadView {
     [super loadView];
     self.backgroundImageView = [[OWKenBurnsView alloc] initWithFrame:self.view.frame];
-    self.blurbLabel.text = @"Welcome to OpenWatch! A social muckraking platform. Enter your email address to get started.";
+    self.logoView.contentMode = UIViewContentModeScaleAspectFit;
+    self.blurbLabel.text = @"Welcome to OpenWatch, a social muckraking platform. Enter your email address to get started.";
     self.blurbLabel.backgroundColor = [UIColor clearColor];
     self.blurbLabel.textColor = [UIColor whiteColor];
     self.blurbLabel.font = [UIFont boldSystemFontOfSize:17.0f];
     self.blurbLabel.shadowColor = [UIColor blackColor];
     self.blurbLabel.shadowOffset = CGSizeMake(0, -1);
     self.blurbLabel.numberOfLines = 0;
-    self.emailField.placeholder = @"Email address";
+    self.emailField.placeholder = @"Email Address";
+    self.emailField.returnKeyType = UIReturnKeyGo;
+    self.emailField.keyboardType = UIKeyboardTypeEmailAddress;
     self.emailField.borderStyle = UITextBorderStyleRoundedRect;
     [self.startButton setTitle:@"Get Started →" forState:UIControlStateNormal];
     [self.startButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [self.startButton setTitleShadowColor:[UIColor blackColor] forState:UIControlStateNormal];
-
-    [self.view addSubview:backgroundImageView];
-    [self.view addSubview:logoView];
-    [self.view addSubview:blurbLabel];
-    [self.view addSubview:emailField];
-    [self.view addSubview:startButton];
-    self.logoView.contentMode = UIViewContentModeScaleAspectFit;
+    [self.startButton setTitleShadowColor:[UIColor grayColor] forState:UIControlStateNormal];
+    self.startButton.titleLabel.shadowOffset = CGSizeMake(0, -1);
+    [self.startButton addTarget:self action:@selector(startButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:scrollView];
+    
+    [self.scrollView addSubview:backgroundImageView];
+    [self.scrollView addSubview:logoView];
+    [self.scrollView addSubview:blurbLabel];
+    [self.scrollView addSubview:emailField];
+    [self.scrollView addSubview:startButton];
 }
 
 - (void)viewDidLoad
@@ -62,6 +71,8 @@
 }
 
 - (void) viewWillAppear:(BOOL)animated {
+    self.scrollView.frame = self.view.bounds;
+    self.scrollView.contentSize = self.view.bounds.size;
     CGFloat xPadding = 20.0f;
     CGFloat yPadding = 20.0f;
     CGFloat logoWidth = self.view.frame.size.width - xPadding * 2;
@@ -73,6 +84,48 @@
     self.backgroundImageView.frame = self.view.bounds;
     [backgroundImageView zoomImageView:backgroundImageView.currentImageView];
     [backgroundImageView startTimer];
+}
+
+- (void) textFieldDidBeginEditing:(UITextField *)textField {
+    [self.scrollView setContentOffset:CGPointMake(0, 145) animated:YES];
+}
+
+- (void) textFieldDidEndEditing:(UITextField *)textField {
+    [self.scrollView setContentOffset:CGPointZero animated:YES];
+}
+
+- (BOOL) textFieldShouldReturn:(UITextField *)textField {
+    [self startButtonPressed:nil];
+    return YES;
+}
+
+- (void) showPasswordField {
+    if (!self.passwordField) {
+        self.passwordField = [[UITextField alloc] init];
+        self.passwordField.delegate = self;
+        self.passwordField.placeholder = @"Password";
+        self.passwordField.secureTextEntry = YES;
+        self.passwordField.returnKeyType = UIReturnKeyGo;
+        self.passwordField.borderStyle = UITextBorderStyleRoundedRect;
+        [self.scrollView addSubview:passwordField];
+        self.passwordField.layer.opacity = 0.0f;
+        CGRect passwordFrame = self.emailField.frame;
+        passwordFrame.origin.y = [OWUtilities bottomOfView:emailField] + 20;
+        self.passwordField.frame = passwordFrame;
+        [UIView animateWithDuration:0.5 animations:^{
+            CGRect startButtonFrame = self.startButton.frame;
+            startButtonFrame.origin.y = [OWUtilities bottomOfView:passwordField] + 20;
+            self.startButton.frame = startButtonFrame;
+        }];
+        [UIView animateWithDuration:0.5 delay:0.2 options:nil animations:^{
+            self.passwordField.layer.opacity = 1.0f;
+        } completion:nil];
+    }
+}
+
+- (void) startButtonPressed:(id)sender {
+    [self showPasswordField];
+    //[self.emailField resignFirstResponder];
 }
 
 - (void)didReceiveMemoryWarning
