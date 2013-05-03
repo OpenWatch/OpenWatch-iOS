4fcf7ffc307725285394e4775e102f5dac141e82
Chris Ballinger <chrisballinger@gmail.com>
2013-05-02 17:59:48 -0700

    Add investigation table view cell

---
+++ OpenWatch/OWInvestigationTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  OWInvestigationTableViewCell.h
+//  OpenWatch
+//
+//  Created by Christopher Ballinger on 5/2/13.
+//  Copyright (c) 2013 OpenWatch FPC. All rights reserved.
+//
+
+#import "OWMediaObjectTableViewCell.h"
+
+@interface OWInvestigationTableViewCell : OWMediaObjectTableViewCell
+
+@property (nonatomic, strong) UILabel *blurbLabel;
+
+@end
